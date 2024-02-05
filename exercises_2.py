95f78122fdab4d824782ce2c502efb8dc783cbdc
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-05 16:07:26 +0000

    for loop practice

--- exercises_2.py
+++ exercises_2.py
@@ -18,9 +18,40 @@
 #             break
 #         print(adj, fruit)
 
-fruits = ["apple", "banana", "cherry"]
-for fruit in fruits:
-    if fruit == "banana":
-        continue
-    print(fruit)
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     if fruit == "banana":
+#         continue
+#     print(fruit)
 
+
+
+
+# Python 2 Exercises
+
+## Loops
+
+# for x in range(0, 11):
+#     print(x)
+
+# x = 0
+# while x<11:
+#     print(x)
+#     x += 1
+
+# nums = [0, 2, 8, 20, 43, 82, 195, 204, 367]
+# for x in nums:
+#     print(x)
+    
+# for x in range(0,11):
+#     print(x)
+# else:
+#     print("Done!")
+
+list1 = ["apple", "banana", "cherry", "durian", "elderberry", "fig"]
+list2 = ["avocado", "banana", "coconut", "date", "elderberry", "fig"]
+
+for x in list1:
+    for y in list2:
+        if x == y:
+            print(x)
