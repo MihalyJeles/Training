f52b606cfce59ee7e429bf12daf40ae98384e00b
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-07 18:29:33 +0000

    dictionary first test

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+car = {
+    # key # value
+    'make': 'Jaguar',
+    'model': 'XF',
+    'year' : 2019,
+    'isNew': False
+}
+print (car['make'], car['year'])
+for x in car:
+    print (car[x])
