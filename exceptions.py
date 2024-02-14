581b9c0a63064422e378268af70ef72bbfa3ccaf
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-14 16:54:38 +0000

    try and except test

--- exceptions.py
+++ exceptions.py
@@ -3,3 +3,15 @@ try:
 except:
     print("You can't do that!")
     print("We can still run the program after this though")
+
+
+number = int(input(f'Please give me your age: '))
+
+try:
+    if number > 100:
+        print('The number is bigger than 100!')
+    elif number > 50 and number < 100:
+        print('The number is between 50 and 100!')
+except:
+    print('Please give me a number!')
+
