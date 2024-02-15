76eefbd2d987dfbc3c4f6b636bf768ac2ccb50a0
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-15 06:17:05 +0000

    more practise on exceptions

--- exceptions.py
+++ exceptions.py
@@ -5,13 +5,19 @@
 #     print("We can still run the program after this though")
 
 
-number = int(input(f'Please give me your age: '))
+# number = int(input(f'Please give me your age: '))
 
+# try:
+#     if number > 100:
+#         print('The number is bigger than 100!')
+#     elif number > 50 and number < 100:
+#         print('The number is between 50 and 100!')
+# except:
+#     print('Please give me a number!')
+
+stuff = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 try:
-    if number > 100:
-        print('The number is bigger than 100!')
-    elif number > 50 and number < 100:
-        print('The number is between 50 and 100!')
+    for x in stuff:
+        print (stuff[x + 1])
 except:
-    print('Please give me a number!')
-
+    print ('Something went wrong!')
