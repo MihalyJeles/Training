f72848be107ea68300a3080c7c83c540d27c7b0e
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-18 10:03:53 +0000

    arbitary arguments kwargs

--- functions.py
+++ functions.py
@@ -26,6 +26,14 @@ def my_function(*people):
 
 my_function("Alice", "Bob", "John")
 
+def concatenate(**kwargs):
+    result = ""
+    # Iterating over the Python kwargs dictionary
+    for arg in kwargs.values():
+        result += arg
+    print (result)
+concatenate(a="Real", b="Python", c="Is", d="Great", e="!")
+
 
 
 
