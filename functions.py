d2f388c62cb51543f6dbf4f14898859364a5bc54
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-18 10:02:13 +0000

    arbitrary arguments

--- functions.py
+++ functions.py
@@ -8,9 +8,24 @@ def get_name():
 
 print(get_name())
 
-def my_function(a, b, c):
-    print(a, b, c)
+# def my_function(a, b, c):
+#     print(a, b, c)
 
-my_function('hello', 'world', '!')
+# my_function('hello', 'world', '!')
+
+# def my_function(a, b, c='!'):
+#     print(a, b, c)
+
+# my_function('hello', 'world')
+
+# my_function('hello', 'world', '?')
+
+def my_function(*people):
+    for person in people:
+        print(person)
+
+my_function("Alice", "Bob", "John")
+
+
 
 
