3218b931f7354ac25aeaebb07e820a3d61b67b78
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-19 13:03:30 +0000

    fobinacci sequence

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+# The fibonacci sequence is the sum of the previous two values
+# 1, 1, 2, 3, 5, 8, 13...
+
+position = int(input('Please give me the position of the number from the sequence, what you would like to know!: '))
+
+# def fibonacci(position):
+#     i = 1
+#     while i < position:
+
+def fib(position):
+    if position < 2:
+        return 1
+    return fib(position-2) + fib(position-1) 
+
+print(f'Position: {position}')
+print(fib(position))
