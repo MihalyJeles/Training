da3dc50a67caec85bf247204cd876b8084005d75
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-19 06:00:42 +0000

    practice of **kwargs

--- function_exercise.py
+++ function_exercise.py
@@ -7,11 +7,20 @@
 
 # print(sum(4,5))
 
-def sum(*numbers): 
-    result = 0
-    for num in numbers:
-        result += num
+# def sum(*numbers): 
+#     result = 0
+#     for num in numbers:
+#         result += num
+#     return result
+
+# x = sum(4,5,8,9,3,7)
+# print(x)
+
+def names_dictionary(**names):
+    result = ""
+    for name in names.values():
+        result += name
+    # print(result)
     return result
 
-x = sum(4,5,8,9,3,7)
-print(x)
+print(names_dictionary(a = 'Mihaly', b = 'Viktoria', c = 'Emma', d = 'Dominik'))
