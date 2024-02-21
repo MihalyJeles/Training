9e91f073f204b4da25a54e2d335130154ba831d9
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-21 16:52:34 +0000

    while loop and for loop on a list

--- loops.py
+++ loops.py
@@ -1,2 +1,12 @@
-for x in range(0,11):
+# for x in range(0,11):
+#     print(x)
+
+# x = 0
+# while x < 11:
+#     print(x)
+#     x += 1
+
+nums = [0, 2, 8, 20, 43, 82, 195, 204, 367]
+
+for x in nums:
     print(x)
