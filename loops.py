196cb3e8cfdc6d517e780e4f66168cbf9da4e967
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-21 17:39:58 +0000

    for-else loop

--- loops.py
+++ loops.py
@@ -6,7 +6,12 @@
 #     print(x)
 #     x += 1
 
-nums = [0, 2, 8, 20, 43, 82, 195, 204, 367]
+# nums = [0, 2, 8, 20, 43, 82, 195, 204, 367]
 
-for x in nums:
+# for x in nums:
+#     print(x)
+
+for x in range(0,11):
     print(x)
+else:
+    print('Done')
