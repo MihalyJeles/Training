1d0f60b4815e2092e13a36542d72d7020db57318
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-02-22 04:52:41 +0000

    nested loops

--- loops.py
+++ loops.py
@@ -11,7 +11,15 @@
 # for x in nums:
 #     print(x)
 
-for x in range(0,11):
-    print(x)
-else:
-    print('Done')
+# for x in range(0,11):
+#     print(x)
+# else:
+#     print('Done')
+
+list1 = ["apple", "banana", "cherry", "durian", "elderberry", "fig"]
+list2 = ["avocado", "banana", "coconut", "date", "elderberry", "fig"]
+
+for x in list1:
+    for y in list2:
+        if x == y:
+            print(x)
