fafede4b48d560e1da0b9af3ccd3ba2d891fa1f4
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-04-12 06:29:48 +0100

    practice scopes 2

--- second_week/import.py
+++ second_week/import.py
@@ -1,12 +1,12 @@
-# # import math.factorial
-# n = 23
-# fact = 1
- 
-# for i in range(1, n+1):
-#     fact = fact * i
- 
-# print("The factorial of 23 is : ", end="")
-# print(fact)
+# import math.factorial
+n = 23
+fact = 1
+
+for i in range(1, n+1):
+    fact = fact * i
+
+print("The factorial of 23 is : ", end="")
+print(fact)
 
 def my_function(a):
     b = a - 2
