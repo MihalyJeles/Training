101900943ff0a8ba39c387a0ad3796984e065372
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-04-18 05:51:56 +0100

    modules practice

---
+++ second_week/module1.py
@@ -0,0 +1,6 @@
+def print_name(name):
+    print(name)
+
+def get_a_name_module():
+    x = input("Give me a name:")
+    return x
---
+++ second_week/module2.py
@@ -0,0 +1,6 @@
+import module1
+
+name = module1.get_a_name_module()
+
+module1.print_name(name)
+
