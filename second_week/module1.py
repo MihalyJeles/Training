525c2956d9a12cf3e659f1b2bdde27943ef8b11a
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-04-19 05:19:52 +0100

    practice import 2

--- second_week/module1.py
+++ second_week/module1.py
@@ -1,6 +1,9 @@
 def print_name(name):
     print(name)
 
-def get_a_name_module():
-    x = input("Give me a name:")
-    return x
+def print_age(age):
+    print(age)
+
+# def get_a_name_module():
+#     x = input("Give me a name:")
+#     return x
--- second_week/module2.py
+++ second_week/module2.py
@@ -1,6 +1,13 @@
-import module1
+# import module1
 
-name = module1.get_a_name_module()
+# name = module1.get_a_name_module()
 
-module1.print_name(name)
+# module1.print_name(name)
+
+from module1 import print_name
+print_name("Jane")
+
+from module1 import print_age
+print_age(5)
+
 
