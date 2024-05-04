fcd45d5ea2522e14d16b49e37882098ca85fe661
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-05-04 06:19:57 +0100

    reading csv file into a dictionary

--- third_week/csv_reader.py
+++ third_week/csv_reader.py
@@ -1,6 +1,12 @@
 import csv
 
-with open("test.csv") as file:
-    reader = csv.reader(file, delimiter=',')
-    for row in reader:
+# with open("test.csv") as file:
+#     reader = csv.reader(file, delimiter=',')
+#     for row in reader:
+#         print(row)
+
+import csv
+with open("test.csv", 'r') as file:
+    csv_file = csv.DictReader(file)
+    for row in csv_file:
         print(row)
