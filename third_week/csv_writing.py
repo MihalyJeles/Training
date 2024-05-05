77a06c3f5a188958d1b1514d046a4e29d2bd8ccc
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-05-05 06:22:00 +0100

    writing from a dictionary to a csv file

--- third_week/csv_writing.py
+++ third_week/csv_writing.py
@@ -1,5 +1,15 @@
 import csv
+# with open('test.csv', mode='w') as file:
+#     writer = csv.writer(file, delimiter=',')
+#     writer.writerow(['Joe', 'Bloggs', 40])
+#     writer.writerow(['Jane', 'Smith', 50])
+
 with open('test.csv', mode='w') as file:
-    writer = csv.writer(file, delimiter=',')
-    writer.writerow(['Joe', 'Bloggs', 40])
-    writer.writerow(['Jane', 'Smith', 50])
+    fieldnames = ['first_name', 'last_name', 'age']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({
+    'first_name': 'Jan',
+    'last_name': 'Smith',
+    'age': 60
+    })
