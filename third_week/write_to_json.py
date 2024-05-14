822b5dfde5f41b8f444745033e1d537de6256f9f
Mihaly Jeles <mihaly.jeles@gmail.com>
2024-05-14 06:27:12 +0100

    working with json

---
+++ third_week/write_to_json.py
@@ -0,0 +1,15 @@
+import json
+
+person = {
+    "name": "Rob",
+    "phone": 123456
+}
+# # encode & write to a file
+# with open('example.json', 'w') as f:
+#     json.dump(person, f)
+
+# read file and decode its content
+with open('example.json', 'r') as f:
+    my_data = json.load(f)
+
+print(my_data)
