82bc59c78adb1bca28e17abb178d2dbb8a3eea21
Mihaly Jeles <mihaly.jeles@gmail.com>
2025-06-26 05:32:05 +0100

    download 26

--- Download_file/download.py
+++ Download_file/download.py
@@ -29,7 +29,7 @@ if login_response.ok:
 #             f.write(pdf_response.content)
 #         print("PDF downloaded successfully!")
 #     else:
-#         print("Failed to download PDF.")
+#         print("Failed to download PDF.")f
 # else:
 #     print("Login failed.")
 
